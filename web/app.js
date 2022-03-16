2703255fa02037e613175550addbe8d0e8f4afef
Laurenz Zielinski <laurenz.zielinski@gmail.com>
2022-03-16 09:56:03 +0100

    build_3

--- web/app.js
+++ web/app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 });
 
+app.get('/test', (req, res) => {
+  res.sendFile(__dirname + '/index_test.html')
+});
+
 app.listen(port, () => {
   console.log(`raidshift listening on port ${port}`)
 });
